d595ad504f39e1a0f97801b6d163e612b3d03709
Andrew Fennell <afennell@hotmail.co.uk>
2023-10-27 11:35:02 +0100

    Git gud

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>React Router Dom</h1>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <footer>
+        <p>&copy; Fen the Men (with Ven)</p>
+      </footer>
+    </BrowserRouter>
+  );
+}
+
+export default App;
